79cc7b5be9974f54a8df8bf12cfae5d103c32434
DRACONIIXX <103470426+DRACONIIXX@users.noreply.github.com>
2022-04-11 20:03:01 -0700

    CS updates for Player Controller

---
+++ side_scroller_2020330f1/Assets/_SideScroller/Scripts/PlayerMovement.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public  PlayerController controller;
+    public float sanicRun = 40f;  //speed of runner
+    float horizontalMove = 0f;
+   // bool downCrouch = false;
+   // bool verticalJump = false;
+
+    // Start is called before the first frame update
+   
+
+    // Update is called once per frame
+    void Update() {
+        Debug.Log(horizontalMove = Input.GetAxisRaw("Horizontal") * sanicRun);
+        
+    }
+    void FixedUpdate() { //move character
+        controller.Move(horizontalMove * Time.fixedDeltaTime,false,false);  //Time.deltaTime move same amount no matter how many times function is called. 
+        
+    }
+    
+}
