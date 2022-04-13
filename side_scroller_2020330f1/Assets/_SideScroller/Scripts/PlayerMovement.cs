87787e51165314f09edab6ae0969490494c39300
DRACONIIXX <103470426+DRACONIIXX@users.noreply.github.com>
2022-04-12 19:51:48 -0700

    Player Controller Finished
    Down, Left to Right,  Jump.

--- side_scroller_2020330f1/Assets/_SideScroller/Scripts/PlayerMovement.cs
+++ side_scroller_2020330f1/Assets/_SideScroller/Scripts/PlayerMovement.cs
@@ -7,8 +7,8 @@ public class PlayerMovement : MonoBehaviour
     public  PlayerController controller;
     public float sanicRun = 40f;  //speed of runner
     float horizontalMove = 0f;
-   // bool downCrouch = false;
-   // bool verticalJump = false;
+    bool downCrouch = false;
+    bool verticalJump = false;
 
     // Start is called before the first frame update
    
@@ -16,10 +16,24 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update() {
         horizontalMove = Input.GetAxisRaw("Horizontal") * sanicRun;
-        
+        if (Input.GetButtonDown("Jump"))
+        {
+            verticalJump = true;
+        }
+
+        if (Input.GetButtonDown("Crouch"))
+        {
+            downCrouch =  true;
+        }
+        else if (Input.GetButtonUp("Crouch"))
+        {
+            downCrouch = false;
+        }
     }
     void FixedUpdate() { //move character
-        controller.Move(horizontalMove * Time.fixedDeltaTime,false,false);  //Time.deltaTime move same amount no matter how many times function is called. 
+        controller.Move(horizontalMove * Time.fixedDeltaTime,downCrouch,verticalJump); //Time.deltaTime move same amount no matter how many times function is called. 
+        verticalJump = false;
+        
         
     }
     
